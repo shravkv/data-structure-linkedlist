13f95aadec251f94c0f13e6d13d44d6c6112a6d8
sravan <sravan.k.vodnala@gmail.com>
2022-06-20 19:26:06 +0530

    Added: Adding elements using functon

---
+++ bridgelabz/FunctionClass.java
@@ -0,0 +1,9 @@
+package com.bridgelabz;
+
+public class FunctionClass {
+    public void insertFirstNode(int i) {
+    }
+
+    public void toPrint() {
+    }
+}
--- bridgelabz/LinkedListClass.java
+++ bridgelabz/LinkedListClass.java
@@ -1,16 +1,23 @@
 package com.bridgelabz;
 
+import jdk.internal.icu.text.UnicodeSet;
+
 import java.util.LinkedList;
 
 public class LinkedListClass {
-
-        public static void main(String[] args) {
-            System.out.println("Welcome to LinkedList program with Generics");
-            LinkedList<Integer>list=new LinkedList<>();
-            list.add(56);
-            list.add(30);
-            list.add(70);
-            System.out.println("Simple Linked list is :"+ list);
-
+    
+    public static void main(String[] args) {
+                System.out.println("Welcome to LinkedList program with Generics");
+                LinkedList<Integer>List=new LinkedList<>();
+        UnicodeSet list = null;
+                list.add(56);
+                list.add(30);
+                list.add(70);
+                System.out.println("Simple Linked list is :"+ list);
+                FunctionClass functionClass=new FunctionClass();
+                functionClass.insertFirstNode(70);
+                functionClass.insertFirstNode(30);
+                functionClass.insertFirstNode(56);
+                functionClass.toPrint();
         }
     }
