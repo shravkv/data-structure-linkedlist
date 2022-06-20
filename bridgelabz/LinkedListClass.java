cdcf641306d0b29b49eb4b81ecde38ea4e14b506
shravkv <104706744+shravkv@users.noreply.github.com>
2022-06-20 19:00:03 +0530

    Update LinkedListClass.java

--- bridgelabz/LinkedListClass.java
+++ bridgelabz/LinkedListClass.java
@@ -6,11 +6,6 @@ public class LinkedListClass {
 
         public static void main(String[] args) {
             System.out.println("Welcome to LinkedList program with Generics");
-            LinkedList<Integer>list=new LinkedList<>();
-            list.add(56);
-            list.add(30);
-            list.add(70);
-            System.out.println("Simple Linked list is :"+ list);
-
+           
         }
     }
