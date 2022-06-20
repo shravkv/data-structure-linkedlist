0652ebf2d35dc6f7f72cf856411ae7bb82c5933c
sravan <sravan.k.vodnala@gmail.com>
2022-06-20 18:56:42 +0530

    Added: welcome to linkedlist

---
+++ bridgelabz/LinkedListClass.java
@@ -0,0 +1,16 @@
+package com.bridgelabz;
+
+import java.util.LinkedList;
+
+public class LinkedListClass {
+
+        public static void main(String[] args) {
+            System.out.println("Welcome to LinkedList program with Generics");
+            LinkedList<Integer>list=new LinkedList<>();
+            list.add(56);
+            list.add(30);
+            list.add(70);
+            System.out.println("Simple Linked list is :"+ list);
+
+        }
+    }
