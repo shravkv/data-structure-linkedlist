d96827a74f72f476cbe1e3d958f36324dfa3e73d
sravan <sravan.k.vodnala@gmail.com>
2022-06-22 18:06:55 +0530

    Added: Adding elements using Node and Function class

--- bridgelabz/FunctionClass.java
+++ bridgelabz/FunctionClass.java
@@ -1,18 +1,61 @@
 package com.bridgelabz;
 
 public class FunctionClass {
-    public void insertFirstNode(int i) {
+    Node head;
+
+
+    public void insertFirstNode(int data) {
+        Node node = new Node(data);
+        node.setData(data);
+        node.setNext(head);
+        head = node;
     }
+
+    public void insertLast(int data) {
+        Node currentNode = head;
+        while (currentNode.next != null) {
+            currentNode = currentNode.next;
+        }
+        Node node = new Node(data);
+        node.setData(data);
+        currentNode.setNext(node);
+    }
+
 
     public void toPrint() {
+        Node currentNode = head;
+        System.out.println("Generics LinkedList is :");
+        while (currentNode != null) {
+            currentNode.toPrint();
+            currentNode = currentNode.next;
+        }
     }
 
-    public void appendingNode(int i, int i1) {
+    public void appendingNode(int before, int data) {
+        Node currentNode = head;
+        while (currentNode.data != before) {
+            currentNode = currentNode.next;
+        }
+        Node node = new Node(data);
+        node.next = currentNode.next;
+        currentNode.next = node;
+
     }
 
-    public void insertLast(int i) {
+    public void insertInBetween(int before, int after, int data) {
+        Node currentNode = head;
+        while (currentNode.data != before && currentNode.data != after) {
+            currentNode = currentNode.next;
+        }
+        Node node = new Node(data);
+        node.next = currentNode.next;
+        currentNode.next = node;
     }
 
-    public void insertInBetween(int i, int i1, int i2) {
+
+    public void deleteFirst() {
+        Node currentNode = head;
+        head = head.next;
     }
 }
+
--- bridgelabz/LinkedListClass.java
+++ bridgelabz/LinkedListClass.java
@@ -1,13 +1,17 @@
 package com.bridgelabz;
 
 public class LinkedListClass {
-    
+
     public static void main(String[] args) {
-                System.out.println("Welcome to LinkedList program with Generics");
-                FunctionClass functionClass=new FunctionClass();
-                functionClass.insertFirstNode(56);
-                functionClass.insertLast(70);
-                functionClass.insertInBetween(56, 70, 30);
-                functionClass.toPrint();
-        }
+
+        System.out.println("Welcome to LinkedList program with Generics");
+
+        FunctionClass functionClass = new FunctionClass();
+        functionClass.insertFirstNode(56);
+        functionClass.insertLast(70);
+        functionClass.insertInBetween(56, 70, 30);
+        functionClass.toPrint();
+        functionClass.deleteFirst();
+        functionClass.toPrint();
     }
+}
